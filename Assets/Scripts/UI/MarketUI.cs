4513c8289bd9d8a1a2f823cfa23090a8ceb45417
filatovsj@gmail.com <filatovsj@gmail.com>
2022-07-04 22:48:40 +0300

    add color button

--- Assets/Scripts/UI/MarketUI.cs
+++ Assets/Scripts/UI/MarketUI.cs
@@ -12,21 +12,28 @@ public class MarketUI : MonoBehaviour
    private VisualElement _root;
    private VisualElement _carButtonsContainer;
    private VisualElement _carButton;
-   
-   
+   private List<Material> _carMaterials;
+   public Sprite image;
+
+
 
    private void Start()
    {
+      _carMaterials = transform.gameObject.GetComponent<CarMaterials>().Materials;
       _playerCarsData = Resources.Load<PlayerCarsData>("PlayerCarsData");
       _root = gameObject.GetComponent<UIDocument>().rootVisualElement;
       _carButtonsContainer  = _root.Q<VisualElement>("CarButtonsContainer");
 
-      for (int i = 1; i < _playerCarsData.Data.Count; i++)
+      for (int i = 0; i < _carMaterials.Count; i++)
       {
          VisualElement _template = CarPrefabTemplate.CloneTree();
          _carButton = _template.Q<VisualElement>("CarTemplate");
          _carButtonsContainer.Add(_carButton);
-         _carButton.style.backgroundImage = new StyleBackground(_playerCarsData.Data[i].imageUI);
+         var a = _carMaterials[i].color;
+        
+         
+         _carButton.style.backgroundColor = a;
+         _carButton.name = i.ToString();
       }
    }
 
